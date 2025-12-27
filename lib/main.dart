efcdf58645c759a16c1b1bd4d2a6e559ca02cd01
VNC Macbook Pro M1 <vncmacbookprom1@MacBook-Pro-cua-cellphones.local>
2025-12-27 10:06:05 +0700

    version ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/router/go_router.dart';
@@ -6,14 +7,25 @@ import 'features/authentication/presentation/bloc/auth_bloc.dart';
 import 'firebase_options.dart';
 import 'injection_container.dart';
 
+
+Future<void> initNotification() async {
+  final messaging = FirebaseMessaging.instance;
+
+  await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+}
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  init();
-
+  await initNotification();
+  await init();
   runApp(
       BlocProvider(
         create: (_) => sl<AuthBloc>()..add(AppLoaded()),
--- lib/services/fcm_token_service.dart
+++ lib/services/fcm_token_service.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
@@ -7,7 +9,23 @@ class FcmTokenService {
 
   FcmTokenService(this.messaging, this.firestore);
 
+  /*Future<void> requestNotificationPermission() async {
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }*/
+
   Future<void> saveToken(String userId) async {
+    if(Platform.isIOS){
+      String? apnsToken;
+      apnsToken = await messaging.getAPNSToken();
+
+      if(apnsToken == null){
+        return;
+      }
+    }
     final token = await messaging.getToken();
     if (token == null) return;
 
